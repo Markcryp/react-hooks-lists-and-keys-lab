d823bfb3be955148a2fe0bb39f7e7ba76aa39c98
Mark <mark.njau@student.moringaschool.com>
2024-08-01 23:29:53 +0300

    first commit

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,10 +5,13 @@ function ProjectItem({ name, about, technologies }) {
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      {technologies.map((technologies, id) => {
+        return <span key={id}>{technologies}</span>
+      }
+    )}
+    
     </div>
+  
   );
 }
 
